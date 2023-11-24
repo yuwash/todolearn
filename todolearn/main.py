faeb3af8afdf7d756851f45169c71c79410a2ace
Yushin Washio <yuwash@yandex.com>
2023-11-24 21:02:09 +0100

    Due dates handled in subtasks (using RELATED-TO)
    Allowing extension for review modes.

--- todolearn/main.py
+++ todolearn/main.py
@@ -1,33 +1,114 @@
+import abc
+import bisect
+import collections.abc
 import dataclasses
 import datetime
+import itertools
 import typing
 
 import arrow
 import ics
 
-example_dict = {
-    "card": "die Karte",
-    "learn": "lernen",
-    "correct": "richtig",
-}
+try:
+    from ics import ContentLine
+except ImportError:
+    from ics.grammar.parse import ContentLine
+
+review_modes = {}
+
+Question = str
+Response = str
+
+
+class ReviewMode(abc.ABC):
+    @abc.abstractmethod
+    def get_question(self, card: "ModeCard") -> Question:
+        ...
+
+    @abc.abstractmethod
+    def mark(self, response: Response, correct_response: Response) -> int:
+        ...
+
+    @abc.abstractmethod
+    def rate_difficulty(self, card: "ModeCard") -> int:
+        ...
+
+
+@dataclasses.dataclass
+class ModeCard:
+    root_todo: ics.Todo
+    mode_todo: ics.Todo
+    mode: ReviewMode
+
+    @classmethod
+    def for_mode_todo(self, mode_todo: ics.Todo, card_deck: "CardDeck") -> "ModeCard":
+        mode_name = mode_todo.name
+        mode = review_modes[mode_name]
+        root_uid = get_card_related_to(mode_todo)
+        root_todo = card_deck.get_card_by_uid(root_uid)
+        return ModeCard(
+            root_todo=root_todo,
+            mode_todo=mode_todo,
+            mode=mode,
+        )
+
+    @property
+    def raw_question(self) -> str:
+        return self.root_todo.name
+
+    @property
+    def correct_response(self) -> Response:
+        return self.root_todo.description
+
+    @property
+    def percent(self) -> int:
+        return self.mode_todo.percent
+
+    @property
+    def due(self) -> datetime.datetime:
+        return self.mode_todo.due
+
+    @due.setter
+    def due(self, due: datetime.datetime) -> None:
+        self.mode_todo.due = due
 
 
 @dataclasses.dataclass(frozen=True)
 class Review:
-    card: ics.Todo
-    response: str
-    reviewd_at: datetime.datetime
+    card: ModeCard
+    started_at: datetime.datetime = dataclasses.field(
+        default_factory=lambda: arrow.now().datetime
+    )
 
-    @property
-    def correct_response(self) -> str:
-        return self.card.description
+    def get_question(self) -> Question:
+        return self.card.mode.get_question(self.card)
 
-    def get_delay_time(self) -> datetime.timedelta:
-        previous_delay = (
-            self.reviewd_at - self.card.due
-            if self.card.due is not None else datetime.timedelta()
+
+@dataclasses.dataclass(frozen=True)
+class ReviewResult:
+    review: Review
+    response: Response
+    duration: datetime.timedelta
+    mark: int
+
+    @classmethod
+    def respond(cls, review: Review, response: Response) -> "ReviewResult":
+        mark = review.card.mode.mark(
+            response, review.card.correct_response
         )
-        if self.response == self.correct_response:
+        return cls(
+            review=review,
+            response=response,
+            duration=arrow.now().datetime - review.started_at,
+            mark=mark,
+        )
+
+    def get_next_delay_time(self) -> datetime.timedelta:
+        previous_delay = (
+            self.review.started_at - self.review.card.due
+            if self.review.card.due is not None else datetime.timedelta()
+        )
+        if self.response == self.review.card.correct_response:
             if previous_delay <= datetime.timedelta():
                 return datetime.timedelta(minutes=1)
             return 2 * previous_delay
@@ -37,8 +118,41 @@ class Review:
             return datetime.timedelta(minutes=1)
 
     def update_due(self) -> None:
-        delay_time = self.get_delay_time()
-        self.card.due = self.reviewd_at + delay_time
+        delay_time = self.get_next_delay_time()
+        self.review.card.due = self.review.started_at + delay_time
+
+
+def get_card_related_to(card: ics.Todo) -> typing.Optional[str]:
+    try:
+        content_line: ContentLine = next(
+            content_line for content_line in card.extra
+            if content_line.name == "RELATED-TO"
+        )
+    except StopIteration:
+        return None
+    return content_line.value
+
+
+def set_card_related_to(card: ics.Todo, card_uid: str) -> ContentLine:
+    content_line = ContentLine.parse(f"RELATED-TO:{card_uid}")
+    card.extra.append(content_line)
+    return content_line
+
+
+class FullAnswerReviewMode(ReviewMode):
+    def get_question(self, card: ModeCard) -> Question:
+        return card.raw_question
+
+    def mark(self, response: Response, correct_response: Response) -> int:
+        if response == correct_response:
+            return 1
+        return 0
+
+    def rate_difficulty(self, card: ModeCard) -> int:
+        return 100 - card.percent
+
+
+review_modes["full-answer"] = FullAnswerReviewMode()
 
 
 class CardDeck:
@@ -53,30 +167,131 @@ class CardDeck:
             if card.uid == card_uid
         )
 
+    def get_mode_of_card(self, card_uid: str, mode_name: str) -> ics.Todo:
+        return next(
+            card for card in self.calendar.todos
+            if get_card_related_to(card) == card_uid
+            and card.name == mode_name
+        )
+
+    def add_mode_to_card(
+        self,
+        card_uid: str,
+        mode_name: str,
+        due: datetime.datetime
+    ) -> ics.Todo:
+        mode_todo = ics.Todo(name=mode_name, due=due)
+        set_card_related_to(mode_todo, card_uid)
+        self.calendar.todos.add(mode_todo)
+        return mode_todo
+
+    def select_unlearned_cards(
+        self, max_count: int,
+    ) -> collections.abc.Collection[ics.Todo]:
+        root_cards = []  # Sorted by priority.
+        referenced_card_uids = set()
+        # Referenced cards are those were at least a mode is
+        # already added.
+        for card in self.calendar.todos:
+            referenced_card_uid = get_card_related_to(card)
+            if referenced_card_uid is None:
+                bisect.insort(
+                    root_cards,
+                    (-(card.priority or 0), card),
+                    # Adding the negative priority for sorting.
+                )
+            else:
+                referenced_card_uids.add(referenced_card_uid)
+        return list(itertools.islice(
+            (
+                card for __, card in root_cards
+                if card.uid not in referenced_card_uids
+            ),
+            max_count,
+        ))
+
     def get_next_due_card(
         self,
-        prefer_new_cards_until: datetime.datetime,
-    ) -> ics.Todo:
-        try:
-            return min(
-                (
-                    card for card in self.calendar.todos
-                    if card.due is not None
-                    and card.due < prefer_new_cards_until
-                ),
-                key=lambda card: card.due,
+        mode_names: typing.Container[str],
+        due_until: typing.Optional[datetime.datetime] = None
+    ) -> typing.Optional[ModeCard]:
+        iter_cards = (
+            card for card in self.calendar.todos
+            if card.due is not None and card.name in mode_names
+        )
+        if due_until is not None:
+            iter_cards = (
+                card for card in iter_cards if card.due <= due_until
             )
+        try:
+            mode_todo = min(iter_cards, key=lambda card: card.due)
         except ValueError:
-            pass
-        try:
-            return next(
-                card for card in self.calendar.todos if card.due is None
+            return None
+        return ModeCard.for_mode_todo(mode_todo, self)
+
+    def iter_reviews(
+        self,
+        mode_names: typing.Optional[typing.Container[str]] = None
+    ) -> typing.Iterator[Review]:
+        if mode_names is None:
+            mode_names = review_modes.keys()
+        review_result = None
+        while True:
+            started_at = arrow.now().datetime
+            card = self.get_next_due_card(
+                mode_names=mode_names, due_until=started_at
             )
-        except StopIteration:
-            return min(
-                (card for card in self.calendar.todos if card.due is not None),
-                key=lambda card: card.due,
-            )
+            if card is None:
+                # Try adding new cards before reviewing cards that
+                # aren’t due yet.
+                next_batch = self.select_unlearned_cards(max_count=10)
+                for root_todo in next_batch:
+                    for mode_name in mode_names:
+                        self.add_mode_to_card(
+                            card_uid=root_todo.uid,
+                            mode_name=mode_name,
+                            due=started_at,
+                        )
+                card = self.get_next_due_card(mode_names=mode_names)
+                if card is None:
+                    return
+            review = Review(card=card)
+            response = yield review_result, review
+            if response is None:
+                return
+            review_result = ReviewResult.respond(review, response)
+            review_result.update_due()
+
+
+def interactive_review(card_deck: CardDeck) -> None:
+    ireviews = card_deck.iter_reviews()
+    try:
+        __, review = next(ireviews)
+    except StopIteration:
+        return
+    while True:
+        question = review.get_question()
+        print(f"Front: {question}")
+        response = input(f"Back ([q]uit): ")
+        if response == "q":
+            return
+        review_result, review = ireviews.send(response)
+        last_card = review_result.review.card
+        suffix = (
+            "" if review_result.mark == 1
+            else f" (Correct: {last_card.correct_response})"
+        )
+        if review_result.mark >= 0.8:
+            print(f"🎉{suffix}")
+        else:
+            print(f"❌{suffix}")
+
+
+example_dict = {
+    "card": "die Karte",
+    "learn": "lernen",
+    "correct": "richtig",
+}
 
 
 def make_examples():
@@ -87,24 +302,9 @@ def make_examples():
     return ics.Calendar(todos=todos)
 
 
-def interactive_review(card_deck: CardDeck) -> None:
-    while True:
-        reviewed_at = arrow.now().datetime
-        card = card_deck.get_next_due_card(reviewed_at)
-        print(f"Front: {card.name}")
-        response = input("Back ([q]uit): ")
-        if response == "q":
-            break
-        review = Review(card, response, reviewed_at)
-        if review.response == review.correct_response:
-            print("🎉")
-        else:
-            print(f"❌ (Correct: {review.correct_response})")
-        review.update_due()
-
-
 if __name__ == "__main__":
-    c = make_examples()
-    interactive_review(CardDeck(c))
-    out = c.serialize()
+    cal = make_examples()
+    deck = CardDeck(cal)
+    interactive_review(deck)
+    out = cal.serialize()
     print(out)
