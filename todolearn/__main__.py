d7578740e5137ad2b1324dfe9cfd4633c2e10ac8
Yushin Washio <yuwash@yandex.com>
2023-11-25 16:53:05 +0100

    Enable saving at the end of the session

--- todolearn/__main__.py
+++ todolearn/__main__.py
@@ -21,20 +21,22 @@ def make_examples():
 
 if __name__ == "__main__":
     url = os.environ.get("TODOLEARN_CALDAV_URL")
+    client = None
     if url is not None:
-        username = os.environ["TODOLEARN_CALDAV_USERNAME"]
-        calendar_url = os.environ["TODOLEARN_CALDAV_CALENDAR_URL"]
         password = getpass.getpass("Password: ")
-        cal = caldav.load_from_caldav(
-            dav_url=url,
-            username=username,
+        client = caldav.CaldavClient(
+            url=url,
+            username=os.environ["TODOLEARN_CALDAV_USERNAME"],
+            calendar_url=os.environ["TODOLEARN_CALDAV_CALENDAR_URL"],
             password=password,
-            calendar_url=calendar_url,
         )
+        cal = client.load()
     else:
         example_todos = make_examples()
         cal = ics.Calendar(todos=example_todos)
     deck = review.CardDeck(cal)
     cli.interactive_review(deck)
+    if client is not None:
+        client.save(cal)
     out = cal.serialize()
     print(out)
--- todolearn/caldav.py
+++ todolearn/caldav.py
@@ -1,20 +1,64 @@
+import dataclasses
+
 import caldav
 import ics
 
 
-def load_from_caldav(
-    dav_url: str,
-    username: str,
-    password: str,
-    calendar_url: str,
-) -> ics.Calendar:
-    with caldav.DAVClient(url=dav_url, username=username, password=password) as client:
-        calendar = client.calendar(url=calendar_url)
-        davtodos = calendar.todos()
-    calendars = (ics.Calendar(imports=d.data) for d in davtodos)
-    aggregated_calendar = next(calendars)
-    for calendar in calendars:
-        aggregated_calendar.todos.update(calendar.todos)
-    return aggregated_calendar
+@dataclasses.dataclass
+class CaldavClient:
+    url: str
+    username: str
+    password: str
+    calendar_url: str
 
+    def load(self) -> ics.Calendar:
+        with caldav.DAVClient(url=self.url, username=self.username, password=self.password) as client:
+            calendar = client.calendar(url=self.calendar_url)
+            davtodos = calendar.todos()
+        calendars = (ics.Calendar(imports=d.data) for d in davtodos)
+        aggregated_calendar = next(calendars)
+        for calendar in calendars:
+            aggregated_calendar.todos.update(calendar.todos)
+        return aggregated_calendar
 
+    def save(self, calendar: ics.Calendar) -> None:
+        avoid_overwrite = False
+        with caldav.DAVClient(url=self.url, username=self.username, password=self.password) as client:
+            davcalendar = client.calendar(url=self.calendar_url)
+            for todo in calendar.todos:
+                self._save_todo(davcalendar, todo)
+
+    def _save_todo(self, davcalendar: caldav.Calendar, todo: ics.Todo) -> None:
+        serialized = todo.serialize()
+        existing_todo = davcalendar.todo_by_uid(todo.uid)
+        if existing_todo is None:
+            davcalendar.add_todo(serialized)
+            return
+        existing_todo_as_ics = next(iter(
+            ics.Calendar(imports=existing_todo.data).todos
+        ))
+        if serialized == existing_todo_as_ics.serialize():
+            # Hacky but only way to check for equality.
+            # The todo instance themselves are equal regardless of content.
+            # Comparing the temp_todo from below would always fail as some
+            # header lines are missing.
+            return
+        if not avoid_overwrite:
+            # Very hacky way to create data in python-caldav format.
+            try:
+                save_ = caldav.Todo.save
+                caldav.Todo.save = lambda *args, **kwargs: None
+                temp_todo = davcalendar.add_todo(serialized)
+            finally:
+                caldav.Todo.save = save_
+            existing_todo.data = temp_todo.data
+            try:
+                existing_todo.save(no_create=True)
+                # Never worked with any server, so the usefulness of this
+                # whole block is questionable.
+            except caldav.lib.error.PutError:
+                avoid_overwrite = True
+            else:
+                return
+        existing_todo.delete()
+        davcalendar.add_todo(serialized)
